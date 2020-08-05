2b053091f8d62ca93d613b61ec1501d6dc42d01f
Isidro Morla <euris110811@gmail.com>
2020-08-05 08:40:30 -0500

    My first server with nodejs has been created

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+const http = require('http')
+
+const handleServer = (req, res) => {
+    res.write('<h1>My first server with nodejs</h1>')
+    res.end()
+}
+
+const server = http.createServer(handleServer)
+
+server.listen(3000, () => {
+    console.log('Server on port 3000')
+})
